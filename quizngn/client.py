f56aa579e178fe29c7353631f5a5199004fd8cbd
abahuguna76 <abahuguna76@gmail.com>
2014-04-04 06:54:40 +0000

    added Praveen comment changes and created new branch qzngn_ABv2.0

--- quizngn/client.py
+++ quizngn/client.py
@@ -4,21 +4,19 @@ import requests
 url = 'http://192.168.33.10:5001/api/quizzes'
 headers = {'Content-Type': 'application/json'}
 
-'''
 # Make a POST request to create a quiz in the database.
-#data = dict(title= 'Python Basics 2', difficulty_level='Moderate', text='This quiz tests you on advanced concepts')
-data = dict(title= 'Python Basics updated', difficulty_level = 'Easy updated')
-response = requests.patch(url, data=json.dumps(data), headers=headers)
-'''
+data = dict(title= 'Python Basics 2', difficulty_level='Moderate', text='Explanation 2')
+#data = dict(title= 'Python Basics updated', difficulty_level = 'Easy updated')
+response = requests.post(url, data=json.dumps(data), headers=headers)
 '''
 # Make a POST request to create a question in the database.
+#data = dict(ques_text = "Is this new?", ans_text="This is it", anschoices=[{"answer":"a. New answer CHANGE", "correct":True}, {"answer":"b. Second choice mod", "correct":False}, {"answer":"c. Third modified mod", "correct":True}])                                
 data = dict(ques_text = "Is this new?", ans_text="This is it", anschoices=[{"answer":"a. New answer CHANGE", "correct":True}, {"answer":"b. Second choice mod", "correct":False}, {"answer":"c. Third modified mod", "correct":True}])                                
-#data = dict(ques_text = "Is this new?", ans_text="CHANGE", anschoices=[{"answer":"a. New answer CHANGE", "correct":True}])
 response = requests.post(url, data=json.dumps(data), headers=headers)
 '''
 
 # Make a GET request for the entire collection.
-response = requests.get(url, headers=headers)
+#response = requests.get(url, headers=headers)
 #response = requests.delete(url, headers=headers)
 '''
 # Make a DEL request for the entire collection.
